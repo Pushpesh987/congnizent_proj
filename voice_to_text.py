04f9f98bfe096d79ff307552cb4f46bed2ec5140
Pushpesh Kumar <107409499+Pushpesh987@users.noreply.github.com>
2023-08-16 16:28:12 +0000

    commit

--- voice_to_text.py
+++ voice_to_text.py
@@ -1,27 +1,24 @@
 import speech_recognition as sr
 
-# Initialize the recognizer
 recognizer = sr.Recognizer()
 
-# Function to transcribe speech from an audio file
 def transcribe_audio(audio_file_path):
     with sr.AudioFile(audio_file_path) as source:
-        audio = recognizer.record(source)  # Record the audio file
+        audio = recognizer.record(source)
 
     try:
-        text = recognizer.recognize_google(audio)  # Use Google Web Speech API for transcription
+        text = recognizer.recognize_google(audio)
         return text
     except sr.UnknownValueError:
         return "Speech recognition could not understand audio"
     except sr.RequestError as e:
-        return f"Could not request results from Google Web Speech API; {e}"
+        return f"Could not request results "
 
-# Provide the path to your audio file
+
 audio_file_path = "/content/rec1.wav"
 
-# Call the function to transcribe the speech
 transcribed_text = transcribe_audio(audio_file_path)
 
-# Print the transcribed text
+
 print("Transcribed Text:")
 print(transcribed_text)
